1c89a56f9dadca836b747d6fa16a7cd9522e63d9
javier123454321 <javier.e.glz@gmail.com>
2019-09-12 14:31:36 -0500

    added the UI for a issue report, need to make the send form functionality

--- javascript/claimPage.js
+++ javascript/claimPage.js
@@ -4,7 +4,11 @@
 function getPicture(){
     //to test, input element 3988, it is the only picture on the server.
     //Otherwise, if you dont, it defaults to a picture of a flower, which is not what we want
-
+    if (document.getElementById("claimPhotoModal").style.display == "none"){
+      document.getElementById("claimPhotoModal").style.display = "block";
+      document.getElementById("reportIssueModal").style.display = "none";
+    };
+    document.getElementById("reportIssueModal").style.display = "none";
     let pictureID = document.getElementById("picID").value;
     let imgPath = "/images/downloadables/IMG_" + pictureID + ".jpg";
 
@@ -14,13 +18,19 @@ function getPicture(){
 
     if(pictureID != ""){
         document.getElementsByClassName("previewImg")[0].src = imgPath;
-        document.getElementById("homenav").setAttribute('href', imgPath);
+        document.getElementById("downloadLink").setAttribute('href', imgPath);
         document.getElementById("imglink").setAttribute('href', imgPath);
         console.log("new src = " + document.getElementsByClassName("previewImg")[0].src);
       }
-
-    document.getElementsByClassName("showPic")[0].style.display = "block";
-    // let height1 = document.getElementsByClassName("claimLanding")[0].offsetHeight;
-    // let height2 = (document.getElementsByClassName("previewImg")[0].offsetHeight +350);
-    // document.getElementsByClassName('claimLanding')[0].style.height = (height1 + height2) + 'px';
+    // 
+    // document.getElementsByClassName("showPic")[0].style.display = "block";
     }
+
+let showIssueForm = document.getElementById("reportIssueButton");
+showIssueForm.addEventListener("click", showReportIssueScreen, false);
+
+function showReportIssueScreen(){
+  console.log("showReportIssueScreen called");
+  document.getElementById("claimPhotoModal").style.display = "none";
+  document.getElementById("reportIssueModal").style.display = "block";
+  };
