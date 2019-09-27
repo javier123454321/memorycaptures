e5a799c8e8a1df7efdfb1c3b96ffbd94e825e70f
Javier Gonzalez <Hal@Dr-Jeckyll.lan>
2019-09-27 07:59:54 -0500

    Setting up Cloudinary functionality through their api

--- javascript/claimPage.js
+++ javascript/claimPage.js
@@ -1,6 +1,29 @@
 //Script finds the image based on pitcure ID and displays it along with a download button in the claimPage
     // Script by: Javier Gonzalez javier.e.glz@gmail.com
 
+function findPicture(pictureID){
+  let imgPath = "https://res.cloudinary.com/memorycaptures-com/image/upload/v1569562371/MemoriesCaptured/" + pictureID + ".jpg";
+  if(isAPicture(pictureID)){
+      document.getElementsByClassName("previewImg")[0].src = imgPath;
+      document.getElementById("downloadLink").setAttribute('href', imgPath);
+      document.getElementById("imglink").setAttribute('href', imgPath);
+      console.log("new src = " + document.getElementsByClassName("previewImg")[0].src);
+    }else{
+      document.getElementById("claimModalTitle").innerText = "Picture Not Found";
+    }
+};
+
+function isAPicture(ID){
+  //To Do define if the picture code is valid, return Boolean
+  //For now just checking if ID is an empty string
+  // return (ID != "");
+  return False;
+}
+
+// <a id="downloadLink" class="buttonStyle" href="https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg" download="Captured Memory.jpg" target="_blank">DOWNLOAD LINK</a>
+// <br> <br><br>
+// <a id="imglink" href="https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg"><img class="previewImg" src = "https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg" alt="Captured Memory" title="View Full Resolution Image"></a>
+
 function getPicture(){
     //to test, input element 3988, it is the only picture on the server.
     //Otherwise, if you dont, it defaults to a picture of a flower, which is not what we want
@@ -10,21 +33,11 @@ function getPicture(){
     };
     document.getElementById("reportIssueModal").style.display = "none";
     let pictureID = document.getElementById("picID").value;
-    let imgPath = "/images/downloadables/IMG_" + pictureID + ".jpg";
 
-    console.log("submit button clicked");
-    console.log("imgPath = " + imgPath);
-    console.log("default src = " + document.getElementsByClassName("previewImg")[0].src);
+    findPicture(pictureID);
+  };
 
-    if(pictureID != ""){
-        document.getElementsByClassName("previewImg")[0].src = imgPath;
-        document.getElementById("downloadLink").setAttribute('href', imgPath);
-        document.getElementById("imglink").setAttribute('href', imgPath);
-        console.log("new src = " + document.getElementsByClassName("previewImg")[0].src);
-      }
-    //
-    // document.getElementsByClassName("showPic")[0].style.display = "block";
-    }
+
 
 let showIssueForm = document.getElementById("reportIssueButton");
 showIssueForm.addEventListener("click", showReportIssueScreen, false);
