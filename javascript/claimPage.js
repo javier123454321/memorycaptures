48e9ace9bacd476d68cc520a9fab50f2b90e6429
Javier Gonzalez <Hal@Dr-Jeckyll.lan>
2019-09-28 20:13:34 -0500

    getPicture now plugs into the remote server to display pics

--- javascript/claimPage.js
+++ javascript/claimPage.js
@@ -1,20 +1,23 @@
 //Script finds the image based on pitcure ID and displays it along with a download button in the claimPage
     // Script by: Javier Gonzalez javier.e.glz@gmail.com
-// var cloudinary = require('cloudinary').v2;
+var cloudinary = require('cloudinary').v2;
 
 function findPicture(pictureID){
   let imgPath = "\"https://res.cloudinary.com/memorycaptures-com/image/upload/v1569562371/MemoriesCaptured/IMG_" + pictureID + ".jpg\"";
-  let displayPhotoHTML = '<a id="downloadLink" class="buttonStyle" href='+ imgPath + ' download="Captured Memory.jpg" target="_blank">'
-      + 'DOWNLOAD LINK</a> <br><br><br>' + '<a id="imglink" href=' + imgPath + '>' +
+  let displayPhotoHTML = '<a id="downloadLink" class="buttonStyle" href='+ imgPath +
+      ' download="Captured Memory.jpg" target="_blank">' +
+      'DOWNLOAD LINK</a> <br><br><br>' + '<a id="imglink" href=' + imgPath + '>' +
       '<img class="previewImg" src =' + imgPath + ' alt="Captured Memory" title="View Full Resolution Image"></a>'
-  console.log(displayPhotoHTML);
+
+  let displayErrorHTML = '<h4>Sorry, this happens when the code you entered is incorrect. <br>'+
+                          'You can double check the code or contact us.</h4><br>'
 
   if(isAPicture(pictureID)){
       document.getElementById("claimModalTitle").innerText = "Found Your Memory!";
       document.getElementsByClassName("claimPhoto")[0].innerHTML = displayPhotoHTML;
-      console.log("new src = " + document.getElementsByClassName("previewImg")[0].src);
     }else{
       document.getElementById("claimModalTitle").innerText = "Picture Not Found";
+      document.getElementsByClassName("claimPhoto")[0].innerHTML = displayErrorHTML;
     }
 };
 
@@ -25,10 +28,6 @@ function isAPicture(ID){
   console.log(ID != "");
   return (ID != "");
 }
-
-// <a id="downloadLink" class="buttonStyle" href="https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg" download="Captured Memory.jpg" target="_blank">DOWNLOAD LINK</a>
-// <br> <br><br>
-// <a id="imglink" href="https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg"><img class="previewImg" src = "https://res.cloudinary.com/memorycaptures-com/image/upload/v1566865326/sample.jpg" alt="Captured Memory" title="View Full Resolution Image"></a>
 
 function getPicture(){
     if (document.getElementById("claimPhotoModal").style.display == "none"){
@@ -44,7 +43,8 @@ function getPicture(){
 
 
 let showIssueForm = document.getElementById("reportIssueButton");
-showIssueForm.addEventListener("click", showReportIssueScreen, false);
+showIssueForm.addEventListener("click", function(){
+  showReportIssueScreen()}, false);
 
 function showReportIssueScreen(){
   console.log("showReportIssueScreen called");
